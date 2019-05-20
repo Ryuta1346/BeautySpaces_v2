cc25b89790a5d786963ccca4f59fac0aa1b16ed5
Ryu007 <ryuta.tashiro@gmail.com>
2019-05-20 21:37:48 +0900

    Modify spec/rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,12 @@ RSpec.configure do |config|
   #
   RSpec.configure do |config|
     config.include FactoryBot::Syntax::Methods
+
+    Shoulda::Matchers.configure do |config|
+      config.integrate do |with|
+        with.test_framework :rspec
+        with.library :rails
+      end
+    end
   end
 end
