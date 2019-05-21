0c8ebb68f115b7cdd807eacf71e821082cf9fdc9
Ryu007 <ryuta.tashiro@gmail.com>
2019-05-21 22:40:40 +0900

    Add TopController & Admin::BaseController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'top#home'
+  get '/about', to: 'top#about'
+  get '/help', to: 'top#help'
+  get '/contact', to: 'top#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
