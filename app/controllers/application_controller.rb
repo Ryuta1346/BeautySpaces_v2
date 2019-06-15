47d4c76f5162ae073fa6a40ee55b9c685df7e916
Ryu007 <surveyson1346@gmail.com>
2019-06-15 13:57:51 +0900

    Modify method on application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :type])
     end
 
-    def after_sign_in_path_for(resource)
-      return admin_salon_path if resource.class == Salon
-      return admin_stylist_path if resource.class == Stylist
-      return user_path(resource) if resource.class == User
+    def after_sign_in_path_for(customer)
+      return admin_salon_path if customer.class == Salon
+      return admin_stylist_path if customer.class == Stylist
+      return user_path(customer) if customer.class == User
     end
 end
