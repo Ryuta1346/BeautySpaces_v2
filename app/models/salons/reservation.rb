60faf3eb9270605fd092ac335bdd06a9587f6751
Ryu007 <ryuta.tashiro@gmail.com>
2019-07-03 22:44:17 +0900

    fix: clear the validations for Salos::Reservation

--- app/models/salons/reservation.rb
+++ app/models/salons/reservation.rb
@@ -1,3 +1,7 @@
 class Salons::Reservation < ApplicationRecord
   belongs_to :customer
+
+  validates :reservation_time, :operation_time, presence: true
+  validates :status, inclusion: { in: [true, false] }
+  validates :status, exclusion: { in: [nil] }
 end
