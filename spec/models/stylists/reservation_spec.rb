ee2bf71850ccbdaa3cb2b6ba631adc372e2bb4ef
Ryu007 <ryuta.tashiro@gmail.com>
2019-07-03 22:57:49 +0900

    feat: Sets models_spec for Stylists::Reservation

--- spec/models/stylists/reservation_spec.rb
+++ spec/models/stylists/reservation_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Stylists::Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to belong_to(:customer) }
+    it { is_expected.to validate_presence_of :reservation_time }
+    it { is_expected.to validate_presence_of :operation_time }
+    it { is_expected.to validate_presence_of :activity_scope }
+    it { is_expected.to validate_inclusion_of(:status).in_array([true, false]) }
+    it { is_expected.to validate_exclusion_of(:status).in_array([nil]) }
+  end
 end
