8e1c7e0e29fcd973c4fc08af17500631ae3f628a
Ryu007 <ryuta.tashiro@gmail.com>
2019-07-06 20:06:22 +0900

    fix: install webdrivers gem and uninstall selenium-webdriver gem

--- spec/factories/salons/reservations.rb
+++ spec/factories/salons/reservations.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :salons_reservation, class: 'Salons::Reservation' do
     association :customer
-    sequence(:reservation_time) { |n| "2019-0#{12 - n}-01 12:00:00" }
+    sequence(:reservation_time) { |n| "2019-0#{12-n}-01 12:00:00" }
     operation_time { 60 }
     memo { "MyText" }
     status { false }
