fa6efe9f312dc8679ce8bd21d43c0b7b3b6522ed
Ryu007 <ryuta.tashiro@gmail.com>
2019-07-15 13:05:52 +0900

    feat: Modify views for Stylists::Menu

--- app/controllers/admin/stylists/menus_controller.rb
+++ app/controllers/admin/stylists/menus_controller.rb
@@ -1,21 +1,19 @@
 class Admin::Stylists::MenusController < ApplicationController
   before_action :set_current_stylist
+  before_action :set_menu, except: [:index]
 
   def index
     @menu = @stylist.menus.build
     @menus = @stylist.menus.all
   end
 
-  def show
-  end
-
   def create
     @menu = @stylist.menus.build(stylist_menus_params)
     if @menu.save
-      flash[:success] = "予約可能時間の登録に成功しました"
+      flash[:success] = "メニューの登録に成功しました"
       redirect_to admin_stylist_menus_url
     else
-      flash[:danger] = "予約可能時間の登録に失敗しました"
+      flash[:danger] = "メニューの登録に失敗しました"
       redirect_to admin_stylist_menus_url
     end
   end
@@ -24,9 +22,20 @@ class Admin::Stylists::MenusController < ApplicationController
   end
 
   def update
+    if @menu.update_attributes(stylist_menus_params)
+      flash[:success] = "#{@menu.name}の編集に成功しました"
+      redirect_to admin_stylist_menus_url
+    else
+      flash[:danger] = "#{@menu.name}の編集に失敗しました"
+      render admin_stylist_menus_path
+    end
   end
 
   def destroy
+    if @menu.destroy
+      flash[:success] = "予約をキャンセルしました"
+      redirect_to admin_stylist_menus_url
+    end
   end
 
   private
@@ -34,6 +43,10 @@ class Admin::Stylists::MenusController < ApplicationController
     def set_current_stylist
       redirect_to root_url unless current_customer.type['Stylist']
       @stylist ||= current_customer
+    end
+
+    def set_menu
+      @menu = @stylist.menus.find(params[:id])
     end
 
     def stylist_menus_params
