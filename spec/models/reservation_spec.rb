6ee5c210c85af3b05cec7473f9dc63c42c47b0fe
Ryu007 <ryuta.tashiro@gmail.com>
2019-09-26 01:13:20 +0900

    feat: Modify spec/models/reservation_spec.rb to add test.

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe Reservation, type: :model do
   let(:salon1) { create(:salon, category_id: category1.id, prefecture: prefecture1) }
   let(:stylist1) { create(:stylist, category_id: category1.id, prefecture: prefecture1) }
   let(:stylists_menu1) { create(:stylists_menu, customer: stylist1) }
-  let(:salons_reservation1) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today} 12:00:00") }
-  let(:salons_reservation2) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today + 1.days} 12:00:00") }
-  let(:stylists_reservation1) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today} 12:00:00") }
-  let(:stylists_reservation2) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today + 1.days} 12:00:00") }
+  let(:salons_reservation1) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today} 17:00:00") }
+  let(:salons_reservation2) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today} 12:00:00") }
+  let(:stylists_reservation1) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today} 17:00:00") }
+  let(:stylists_reservation2) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today} 12:00:00") }
   let(:user) { create(:user, prefecture: prefecture1, category_id: category2.id) }
   let!(:reservation1) {
     create(:reservation,
@@ -33,7 +33,7 @@ RSpec.describe Reservation, type: :model do
   end
 
   it "returns lists that display today's reservation information" do
-    expect(reservation_info(salon1).only_today.size).to eq 1
+    expect(reservation_info(salon1).only_today.size).to eq 2
     expect(reservation_info(salon1).only_today.first.salons_reservation.reservation_time).to eq reservation1.salons_reservation.reservation_time
   end
 
@@ -41,7 +41,8 @@ RSpec.describe Reservation, type: :model do
     expect(reservation_info(salon1).only_today.first.salons_reservation.reservation_time).not_to eq reservation2.salons_reservation.reservation_time
   end
 
-  it "returns reservation's schedule" do
+  it "returns reservation's schedule in the correct order" do
+    expect(reservation_info(salon1).only_today.order(reservation_time: :asc).first).to eq reservation2
   end
 
   def reservation_info(salon)
