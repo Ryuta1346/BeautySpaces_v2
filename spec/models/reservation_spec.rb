23b5ba343a022cd098c957d701022ca5d3b4ac65
Ryu007 <ryuta.tashiro@gmail.com>
2019-09-30 22:06:30 +0900

    fix: Modify reservation_spec.rb to solve the RSpec's problem.

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  let(:category1) { create(:category, name: "ヘアサロン") }
-  let(:category2) { create(:category, name: "") }
-  let(:region1) { create(:region) }
-  let(:prefecture1) { create(:prefecture, region: region1) }
-  let(:salon1) { create(:salon, category_id: category1.id, prefecture: prefecture1) }
-  let(:stylist1) { create(:stylist, category_id: category1.id, prefecture: prefecture1) }
-  let(:stylists_menu1) { create(:stylists_menu, customer: stylist1) }
-  let(:salons_reservation1) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today} 17:00:00") }
-  let(:salons_reservation2) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today} 12:00:00") }
-  let(:stylists_reservation1) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today} 17:00:00") }
-  let(:stylists_reservation2) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today} 12:00:00") }
-  let(:user) { create(:user, prefecture: prefecture1, category_id: category2.id) }
+  let!(:category1) { create(:category, name: "ヘアサロン") }
+  let!(:category2) { create(:category, name: "") }
+  let!(:region1) { create(:region) }
+  let!(:prefecture1) { create(:prefecture, region: region1) }
+  let!(:salon1) { create(:salon, category_id: category1.id, prefecture: prefecture1) }
+  let!(:stylist1) { create(:stylist, category_id: category1.id, prefecture: prefecture1) }
+  let!(:stylists_menu1) { create(:stylists_menu, customer: stylist1) }
+  let!(:salons_reservation1) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today} 17:00:00") }
+  let!(:salons_reservation2) { create(:salons_reservation, customer: salon1, reservation_time: "#{Date.today} 12:00:00") }
+  let!(:stylists_reservation1) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today} 17:00:00") }
+  let!(:stylists_reservation2) { create(:stylists_reservation, customer: stylist1, reservation_time: "#{Date.today} 12:00:00") }
+  let!(:user) { create(:user, prefecture: prefecture1, category_id: category2.id) }
   let!(:reservation1) {
     create(:reservation,
            salons_reservation:   salons_reservation1,
